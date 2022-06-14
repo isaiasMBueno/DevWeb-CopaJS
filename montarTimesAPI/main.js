a5c1f9bbd667b77bb99f78723682d365b4c1f75b
isaiasMBueno <isaiasmbueno@gmail.com>
2022-06-14 15:35:51 -0300

    finalRelease

--- montarTimesAPI/main.js
+++ montarTimesAPI/main.js
@@ -1,7 +1,8 @@
 var _allTeams = [];
 var _allPlayers = [];
 var _selectedTeam = "";
-var count = 1;
+var _selectedPlayers = [];
+var _count = 1;
 function LoadForm() {
 
   for (let value in posicoesConfig.Posicoes) {
@@ -49,10 +50,49 @@ function setPosicaoJogadores(value) {
 }
 
 function GetNome(idValue, positionValue) {
-  if (_selectedTeam == null || _selectedTeam == "") {
-    document.getElementById(idValue).title = "?";
-    return ('?');
+  let nome= "";
+  if (_selectedPlayers.length < 11) {
+    switch(idValue) {
+      case 'b1':
+        nome = 'Goleiro';
+        break;
+      case 'b2':
+        nome = 'Zagueiro 1';
+        break;
+      case 'b3':
+        nome = 'Zagueiro 2';
+        break;
+      case 'b4':
+        nome = 'Zagueiro 3';
+        break;
+      case 'b5':
+        nome = 'Zagueiro 4';
+        break;
+      case 'b6':
+        nome = 'Meio-Campo 1';
+        break;
+      case 'b7':
+        nome = 'Meio-Campo 2';
+        break;
+      case 'b8':
+        nome = 'Meio-Campo 3';
+        break;        
+      case 'b9':
+        nome = 'Atacante 1';
+        break;
+      case 'b10':
+        nome = 'Atacante 2';
+        break;
+      case 'b11':
+        nome = 'Atacante 3';
+        break;             
+    }
+  }else{
+    nome = _selectedPlayers[positionValue].display_name;
   }
+
+  document.getElementById(idValue).title = nome;
+  return (nome);
 }
 
 function GetPosicaoJogador(value) {
@@ -82,18 +122,22 @@ function GetPosicaoJogador(value) {
   }
 }
 
-function onClickJogador(idValue, positionValue) {
-  // var nomeJogador = this.GetNome(idValue, positionValue).replace(" ", "+");
-  // var posicaoJogador = this.GetPosicaoJogador(_allTeams[_selectedTeam].Jogadores[positionValue].Posicao);
-  // window.open(
-  //   'https://www.google.com/search?q=' + nomeJogador + "+" + posicaoJogador + "+seleção+" + _selectedTeam,
-  //   "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,width=900,height=500");
-}
 
 function onChangeValueSelect(selectedValue) {
   document.getElementById("divTime").innerHTML = "";
-  _selectedTeam = _allTeams.find(x => x.id == selectedValue.value);
+  _allPlayers = [];
+  _selectedPlayers = [];
+  _count = 1;
 
+  if(!selectedValue.value){
+    document.getElementById("nomeTime").innerText = "";
+    pushTablePlayers(null);
+    return;
+  }
+
+  _selectedTeam = _allTeams.find(x => x.id == selectedValue.value);
+  
+  document.getElementById("nomeTime").innerText = _selectedTeam.name;
 
 
 
@@ -101,10 +145,6 @@ function onChangeValueSelect(selectedValue) {
 
   bd.innerHTML = _selectedTeam.extra.flag;
 
-
-
-  bd.setAttribute("widht", "150px");
-  bd.setAttribute("heigth", "94px");
 
   if (_selectedTeam == null || _selectedTeam == "" || _selectedTeam.extra == null) {
   } else {
@@ -122,34 +162,28 @@ function onChangeValueSelect(selectedValue) {
         }
       });
   });
-
-  // pushTablePlayers();
-
-
 }
 
 function pushTablePlayers(value){
   let listJogadores = document.getElementById('listJogadores');
   listJogadores.innerHTML = "";
 
-  value = value.sort((a, b) => { return a.position_id - b.position_id;});
+  if(!value){
+    return;
+  }
 
-  console.log(value);
+  value = value.sort((a, b) => { return a.position_id - b.position_id;});
 
   for(let i = 1; i < value.length; i++)
   {
-
-    if (i < 12) {
-      document.getElementById('b' + i).style.backgroundImage = "url(" + value[i].image_path + ")";
-    }
-
     let linha = document.createElement('tr');
     linha.addEventListener('click', function () {
-      selectJogador(i);
+      document.getElementById('b' + _count).classList.add("focus");
+      selectJogador(value[i].player_id);
     });
 
     let col1 = document.createElement('td');
-    col1.innerText = i;
+    col1.innerText = value[i].player_id;
 
     let col2 = document.createElement('td');
     col2.innerText = value[i].display_name;
@@ -177,4 +211,28 @@ function pushTablePlayers(value){
 function onChangeValueSelectFormacao(selectedValue) {
   document.getElementById("posicao").innerText = selectedValue.value;
   setPosicaoJogadores(selectedValue.value);
+}
+
+function selectJogador(value){
+  if(_count == 12){
+    _count = 1;
+  }
+
+  if(window.confirm("Deseja substituir o " + GetNome('b'+_count, _count) + "?")){
+    let jogador = document.getElementById('b'+_count);
+    
+    let dadosJogador = _allPlayers.find(x => x.player_id == value);
+    _selectedPlayers.push(dadosJogador);
+    jogador.style.backgroundImage = "url("+dadosJogador.image_path+")";
+    jogador.title = jogador.display_name;
+    document.getElementById('b' + _count).classList.remove("focus");
+    _count++;
+  }
+}
+
+function onClickJogador(idValue, positionValue) {
+  var nomeJogador = this.GetNome(idValue, positionValue);
+  window.open(
+    'https://www.google.com/search?q=' + nomeJogador + "+seleção+" + _selectedTeam.name,
+    "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,width=900,height=500");
 }
--- posicionamentoTimes/main.js
+++ posicionamentoTimes/main.js
@@ -1,23 +1,24 @@
 var _allTeams = {
-   "Argentina": Argentina, "Bolivia": Bolivia, "Brasil": Brasil,
-   "Chile": Chile, "Colombia": Colombia, "Equador": Equador,
-   "Paraguai": Paraguai, "Peru": Peru, "Uruguai": Uruguai, "Venezuela": Venezuela};
+  "Argentina": Argentina, "Bolivia": Bolivia, "Brasil": Brasil,
+  "Chile": Chile, "Colombia": Colombia, "Equador": Equador,
+  "Paraguai": Paraguai, "Peru": Peru, "Uruguai": Uruguai, "Venezuela": Venezuela
+};
 var _selectedTeam = "";
 
 function LoadForm() {
   const selectTime = document.getElementById("selectTime");
   const selectFormacao = document.getElementById("selectFormacao");
 
-  for (let value in _allTeams) { 
+  for (let value in _allTeams) {
     const newOption = new Option(value);
-    newOption.setAttribute('data-thumbnail' ,'assets/flags/brazil.svg')
-    selectTime.append(newOption); 
+    newOption.setAttribute('data-thumbnail', 'assets/flags/brazil.svg')
+    selectTime.append(newOption);
   }
-  
+
   for (let value in posicoesConfig.Posicoes) {
     selectFormacao.append(new Option(posicoesConfig.Posicoes[value].NomePosicao));
   }
-  
+
   setPosicaoJogadores("");
   if (_selectedTeam != null) {
     setJogadoresImage();
@@ -29,7 +30,7 @@ LoadForm();
 function setJogadoresImage() {
   let listJogadores = document.getElementById('listJogadores');
   listJogadores.innerHTML = "";
-  
+
   if (_selectedTeam == null || _selectedTeam == "") {
     for (i = 1; i < 12; i++) {
       document.getElementById('b' + i).style.backgroundImage = "url('./assets/default.png')";
@@ -38,113 +39,112 @@ function setJogadoresImage() {
   }
   var count = 1;
   _allTeams[_selectedTeam].Jogadores.map(jogador => {
-    
+
     document.getElementById('b' + count).style.backgroundImage = jogador.Imagem;
-    
+
     let linha = document.createElement('tr');
     linha.addEventListener('click', function () {
       selectJogador(jogador.Numero);
     });
-    
+
     let col1 = document.createElement('td');
     col1.innerText = jogador.Numero;
-    
+
     let col2 = document.createElement('td');
     col2.innerText = jogador.Nome;
-    
+
     let col3 = document.createElement('td');
     col3.innerText = GetPosicaoJogador(jogador.Posicao);
-    
+
     linha.append(col1, col2, col3)
     listJogadores.appendChild(linha);
     count++;
   })
 }
-  
-  function setPosicaoJogadores(value) {
-    if (value == "null" || value == "") {
-      for (i = 1; i < 12; i++) {
-        document.getElementById('b' + i).style.display = "none";
-      }
+
+function setPosicaoJogadores(value) {
+  if (value == "null" || value == "") {
+    for (i = 1; i < 12; i++) {
+      document.getElementById('b' + i).style.display = "none";
+    }
+    return;
+  }
+  posicoesConfig.Posicoes.map(posicao => {
+    if (posicao.NomePosicao == value) {
+      posicao.Config.map(config => {
+        var jogador = document.getElementById(config.Id);
+        jogador.style.display = "block";
+        jogador.style.left = config.Left;
+        jogador.style.top = config.Top;
+        jogador.style.position = "absolute";
+      });
       return;
     }
-    posicoesConfig.Posicoes.map(posicao => {
-      if (posicao.NomePosicao == value) {
-        posicao.Config.map(config => {
-          var jogador = document.getElementById(config.Id);
-          jogador.style.display = "block";
-          jogador.style.left = config.Left;
-          jogador.style.top = config.Top;
-          jogador.style.position = "absolute";
-        });
-        return;
-      }
-    });
-  }
-  
-  function GetNome(idValue, positionValue) {
-    if (_selectedTeam == null || _selectedTeam == "") {
-      document.getElementById(idValue).title = "?";
-      return ('?');
-    }
-    
-    document.getElementById(idValue).title = (
-      _allTeams[_selectedTeam].Jogadores[positionValue].Nome
-      + ", "
-      + GetPosicaoJogador(_allTeams[_selectedTeam].Jogadores[positionValue].Posicao));
-      return (_allTeams[_selectedTeam].Jogadores[positionValue].Nome);
-    }
-    
-    function GetPosicaoJogador(value) {
-      switch (value) {
-        case "gol":
-        return "Goleiro";
-        case "lat":
-        return "Lateral";
-        case "zag":
-        return "Zagueiro";
-        case "mei":
-        return "Meio-Campo";
-        case "ata":
-        return "Atacante";
-        case "vol":
-        return "Volante";
-        case "cen":
-        return "Centro-Avante";
-      }
-    }
-    
-    function onClickJogador(idValue, positionValue) {
-      var nomeJogador = this.GetNome(idValue, positionValue).replace(" ", "+");
-      var posicaoJogador = this.GetPosicaoJogador(_allTeams[_selectedTeam].Jogadores[positionValue].Posicao);
-      window.open(
-        'https://www.google.com/search?q=' + nomeJogador + "+" + posicaoJogador + "+seleção+" + _selectedTeam,
-        "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,width=900,height=500");
-      }
-      
-      function onChangeValueSelect(selectedValue) {
-        _selectedTeam = selectedValue.value;
-        document.getElementById("nomeTime").innerText = _selectedTeam;
-        if (_selectedTeam == null || _selectedTeam == "") {
-          document.getElementById("divTime").src = "assets/default.jpg";
-          document.getElementById("divBrasao").src = "assets/default.jpg";
-        } else {
-          document.getElementById("divTime").src = _allTeams[_selectedTeam].ImagemTime;
-          document.getElementById("divBrasao").src = _allTeams[_selectedTeam].BrasaoTime;
-        }
-        setJogadoresImage();
-      }
-      
-      function onChangeValueSelectFormacao(selectedValue) {
-        document.getElementById("posicao").innerText = selectedValue.value;
-        setPosicaoJogadores(selectedValue.value);
-      }
-      
-      function selectJogador(count){
-        for(let i=1; i<11; i++){
-          document.getElementById('b'+i).classList.remove("focus");
-        }
+  });
+}
 
-        document.getElementById('b'+count).classList.add("focus");
-      }
-      
+function GetNome(idValue, positionValue) {
+  if (_selectedTeam == null || _selectedTeam == "") {
+    document.getElementById(idValue).title = "?";
+    return ('?');
+  }
+
+  document.getElementById(idValue).title = (
+    _allTeams[_selectedTeam].Jogadores[positionValue].Nome
+    + ", "
+    + GetPosicaoJogador(_allTeams[_selectedTeam].Jogadores[positionValue].Posicao));
+  return (_allTeams[_selectedTeam].Jogadores[positionValue].Nome);
+}
+
+function GetPosicaoJogador(value) {
+  switch (value) {
+    case "gol":
+      return "Goleiro";
+    case "lat":
+      return "Lateral";
+    case "zag":
+      return "Zagueiro";
+    case "mei":
+      return "Meio-Campo";
+    case "ata":
+      return "Atacante";
+    case "vol":
+      return "Volante";
+    case "cen":
+      return "Centro-Avante";
+  }
+}
+
+function onClickJogador(idValue, positionValue) {
+  var nomeJogador = this.GetNome(idValue, positionValue).replace(" ", "+");
+  var posicaoJogador = this.GetPosicaoJogador(_allTeams[_selectedTeam].Jogadores[positionValue].Posicao);
+  window.open(
+    'https://www.google.com/search?q=' + nomeJogador + "+" + posicaoJogador + "+seleção+" + _selectedTeam,
+    "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,width=900,height=500");
+}
+
+function onChangeValueSelect(selectedValue) {
+  _selectedTeam = selectedValue.value;
+  document.getElementById("nomeTime").innerText = _selectedTeam;
+  if (_selectedTeam == null || _selectedTeam == "") {
+    document.getElementById("divTime").src = "assets/default.jpg";
+    document.getElementById("divBrasao").src = "assets/default.jpg";
+  } else {
+    document.getElementById("divTime").src = _allTeams[_selectedTeam].ImagemTime;
+    document.getElementById("divBrasao").src = _allTeams[_selectedTeam].BrasaoTime;
+  }
+  setJogadoresImage();
+}
+
+function onChangeValueSelectFormacao(selectedValue) {
+  document.getElementById("posicao").innerText = selectedValue.value;
+  setPosicaoJogadores(selectedValue.value);
+}
+
+function selectJogador(count) {
+  for (let i = 1; i < 11; i++) {
+    document.getElementById('b' + i).classList.remove("focus");
+  }
+
+  document.getElementById('b' + count).classList.add("focus");
+}
